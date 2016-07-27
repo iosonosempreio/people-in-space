ec1c830afe3afc23bc37e164bdc819c6504c0518
iosonosempreio <tommaso.elli@gmail.com>
2016-07-27 12:14:39 +0200

    general improvements

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-# How many people are there in the space now?
+# People in Space. Now.
 
 Just a first experiment with the incredible <a href="https://p5js.org">p5js</a> library.
 
-The application connects with a [service](https://api.open-notify.org/) that provides in real time the number of astronauts that are on some galactic mission in this precise moment and plot them as floating dots scattered across the space of the window.
+The application connects with a [service](https://peopleinspace.herokuapp.com/) that provides real-time information of the astronauts that are on some galactic mission in this precise moment and plot them as floating coloured circles scattered across the space of the window.
 
 ###Cool stuff used
 - <a href="https://p5js.org">p5js</a>
 - <a href="https://github.com/technoboy10/crossorigin.me">crossorigin.me</a>
+- [moment.js](http://momentjs.com/)
+-[Flag Icon CSS](https://github.com/lipis/flag-icon-css)
--- sketch.js
+++ sketch.js
@@ -1,21 +1,19 @@
 var positions = [],
-    img,
     myFontSize = 14,
     palette = ['4882CC','FFFFC4','9F4B26','F3B404','C7578E'],
-    introText = '<div class="only-big"><p>This experiment is realised thanks to the awesome <a href="https://p5js.org">p5js</a> library.</p><p>The application connects with a <a href="https://api.open-notify.org/">service</a> that provides in real time the number of astronauts that are on some galactic mission in this precise moment and plot them as floating dots scattered across the space of the window.</p></div><p>Discover more on the <a href="Github Repo">GitHub repo</a>.</p>',
-    title = 'How many people are there in the Outer Space, <b>NOW</b>?',
     textBoxHeight,
     speed = 3,
-    maxSize = 80,
-    realMax= 0;
+    minSize = 30,
+    maxSize = 100,
+    realMax= 0,
+    today = moment(),
+    sidebarClosed=true,
+    availableWidth;
 
-function preload() {
-  // var someText = createDiv('<h1>'+title+'</h1><p>'+introText+'</p>');
-  // someText.id('text-box')
-
-  var people = 'https://peopleinspace.herokuapp.com/'
-  loadJSON(people, doThings, manageErrors, 'json');
-  
+function unHexColours(hex) {
+  hex = hex.match(/.{1,2}/g)
+  hex = unhex(hex)
+  return hex
 }
 
 function doThings(data){
@@ -29,60 +27,62 @@ function manageErrors(data){
   console.log(data)
 }
 
-function unHexColours(hex) {
-  hex = hex.match(/.{1,2}/g)
-  hex = unhex(hex)
-  return hex
+function preload() {
+  var people = 'https://peopleinspace.herokuapp.com/'
+  loadJSON(people, doThings, manageErrors, 'json')
 }
 
 function setup() {
 
-  textBoxHeight = parseInt(window.getComputedStyle(document.getElementById("text-box"), null).height)
+  textBoxHeight = int(select('#text-box').style('height'))
 
   if (window.innerWidth<768) {
-    canvas = createCanvas(window.innerWidth, window.innerHeight*0.75)
     speed = 2
-  } else {
-    canvas = createCanvas(window.innerWidth,(window.innerHeight-textBoxHeight))
+    minSize = 15
+    maxSize = 70
+  } else if (window.innerWidth>=1200) {
+    toggleSidebar('about')
   }
-  canvas.position(0, textBoxHeight);
-  // frameRate(30)
-  var today = moment();
-  var colNum = 0;
 
+  canvas = createCanvas(windowWidth,(windowHeight-textBoxHeight))
+  canvas.position(0, textBoxHeight);
+  frameRate(60)
+  var colNum = 0
 
   for(var i = 0; i < people.number; i = i + 1){
 
-      //speed of each circle (x and y)
-      var spedx = random(-speed,speed);
-      var spedy = random(-speed,speed);
+    //speed of each circle (x and y)
+    var spedx = random(-speed,speed)
+    var spedy = random(-speed,speed)
 
-      //assign values
+    //assign values
     positions.push({
       x:random(width*0.2,width*0.8),
       y:random(height*0.2,height*0.8),
-      size: today.diff(moment(people.people[i].launchdate),'days'), //just for now, later this will be mapped to constrain the size according to a mazimum limit
+      size: today.diff(moment(people.people[i].launchdate),'days'),
       myspeedx : spedx,
       myspeedy : spedy,
       speedx:spedx,
       speedy:spedy,
-      colour: unHexColours(palette[colNum])
+      colour: (people.people[i].title == 'Commander') ? unHexColours(palette[3]) : unHexColours(palette[0])
     })
-    colNum++;
+
+    colNum++
+
     if (colNum >= palette.length) {
-      colNum = 0;
+      colNum = 0
     }
 
     //update the maximum value found for now
     if(positions[i].size > realMax) {
-        realMax = positions[i].size;
+      realMax = positions[i].size
     }
   }
 
-    //assign the mapped size for each circle
-    positions.forEach(function(d){
-        d.size = map(d.size, 0, realMax, 0, maxSize);
-    })
+  //assign the mapped size for each circle
+  positions.forEach(function(d){
+      d.size = map(d.size, 0, realMax, minSize, maxSize)
+  })
 
 }
 
@@ -90,81 +90,143 @@ function draw() {
 
   background(26,23,40)
 
+  availableWidth = sidebarClosed ? windowWidth : (windowWidth-int(select('#sidebar').style('width')))
+
   positions.forEach(function(d,i){
-    noStroke();
+    // Draw circles
+    stroke(26,23,40)
     fill(d.colour)
     ellipse(d.x,d.y,d.size,d.size)
-
-    fill(255); 
-    textFont("Droid Sans");
+    // Draw texts
+    fill(255) 
+    textFont("Droid Sans")
     textStyle(BOLD);
     textAlign(CENTER);
     textSize(myFontSize);
-    text(people.people[i].name.toUpperCase(), d.x, d.y+d.size*0.5+myFontSize+2); // Text wraps within text box
+    text(people.people[i].name.toUpperCase(), d.x, d.y+d.size*0.5+myFontSize+2) // Text wraps within text box
 
+    // Update position according to speed and space available when sidebar is open
+    d.x = d.x + d.speedx
+    d.y = d.y + d.speedy
+    if (availableWidth < 1) {
+      select('canvas').style('left', -windowWidth+'px')
+    } else {
+      select('canvas').style('left', '0px')
+    }
+  
 
-      //update position according to velocity
-      d.x = d.x + d.speedx
-      d.y = d.y + d.speedy
-
-   //create vectors to compute distance (using the dist function)
+    // Create vectors to compute distance (using the dist function)
     var currPoint = createVector(d.x, d.y);
     var mousePoint = createVector(mouseX, mouseY);
 
 
-      //update coordinates relative to the distance from the mouse pointer
-      if (currPoint.dist(mousePoint) <= 50) {
-
-          //slow down speed according to mouse pointer
-          d.speedx = d.speedx >= 0 ? d.speedx - .3*Math.abs(d.myspeedx)*(1 / currPoint.dist(mousePoint)) :d.speedx + .3*Math.abs(d.myspeedx)*(1 / currPoint.dist(mousePoint));
-          d.speedy = d.speedy >= 0 ? d.speedy - .3*Math.abs(d.myspeedy)*(1 / currPoint.dist(mousePoint)) :d.speedy + .3*Math.abs(d.myspeedy)*(1 / currPoint.dist(mousePoint));
+    // Update coordinates relative to the distance from the mouse pointer
+    if (currPoint.dist(mousePoint) <= 50) {
+        // Slow down speed according to mouse pointer
+        d.speedx = d.speedx >= 0 ? d.speedx - .3*Math.abs(d.myspeedx)*(1 / currPoint.dist(mousePoint)) :d.speedx + .3*Math.abs(d.myspeedx)*(1 / currPoint.dist(mousePoint));
+        d.speedy = d.speedy >= 0 ? d.speedy - .3*Math.abs(d.myspeedy)*(1 / currPoint.dist(mousePoint)) :d.speedy + .3*Math.abs(d.myspeedy)*(1 / currPoint.dist(mousePoint));
+    } else {
+      // Check collision with borders, handle special cases for the X axis
+      if((d.x+(d.size/2))+36 > availableWidth && d.speedx >= 0 )
+      {
+        d.speedx =  Math.abs(d.myspeedx) * -1;
+      }
+      else if((d.x-(d.size/2))-36 < 0 && d.speedx < 0){
+        d.speedx =  Math.abs(d.myspeedx);
       }
       else {
-
-
-          //check collision with borders, handle special cases for the X axis
-          if((d.x+(d.size/2))+36 > width && d.speedx >= 0 )
-          {
-            d.speedx =  Math.abs(d.myspeedx) * -1;
-          }
-          else if((d.x-(d.size/2))-36 < 0 && d.speedx < 0){
-            d.speedx =  Math.abs(d.myspeedx);
-          }
-
-          else {
-              d.speedx = d.speedx >= 0 ?  Math.abs(d.myspeedx) :  Math.abs(d.myspeedx) * -1;
-          }
-          //----------------
-
-          //check collision with borders, handle special cases for the Y axis
-          if ((d.y+(d.size/2))+myFontSize > height && d.speedy >= 0){
-              d.speedy =  Math.abs(d.myspeedy) * -1;
-          }
-          else if  ((d.y-(d.size/2)) < 0 && d.speedy < 0) {
-              d.speedy =  Math.abs(d.myspeedy);
-          }
-          else {
-              d.speedy = d.speedy >= 0 ?  Math.abs(d.myspeedy) :  Math.abs(d.myspeedy) * -1;
-          }
-          //----------------
+        d.speedx = d.speedx >= 0 ?  Math.abs(d.myspeedx) :  Math.abs(d.myspeedx) * -1;
       }
+      //----------------
+
+      // Check collision with borders, handle special cases for the Y axis
+      if ((d.y+(d.size/2))+myFontSize > height && d.speedy >= 0){
+        d.speedy =  Math.abs(d.myspeedy) * -1;
+      }
+      else if  ((d.y-(d.size/2)) < 0 && d.speedy < 0) {
+        d.speedy =  Math.abs(d.myspeedy);
+      }
+      else {
+        d.speedy = d.speedy >= 0 ?  Math.abs(d.myspeedy) :  Math.abs(d.myspeedy) * -1;
+      }
+      //----------------
+    }
   })
 
 }
 
+function windowResized() {
+  textBoxHeight = int(select('#text-box').style('height'))
+  resizeCanvas(windowWidth,(windowHeight-textBoxHeight))
+  canvas.position(0, textBoxHeight)
+}
 
 function mousePressed() {
-
-//loop through all circles
+// Loop through all circles
  for(var i = 0; i< positions.length; i++){
     var d = positions[i];
+    
+    // Look for the first circle that eventually contains the mouse pointer
+    if( mouseX > (d.x - d.size/2) && mouseX < (d.x + d.size/2) && mouseY > (d.y - d.size/2) && mouseY < (d.y + d.size/2) ) {
+      changeInformation(people.people[i])
 
-     //look for the first circle that eventually contains the mouse pointer
-    if(mouseX > (d.x - d.size/2) && mouseX < (d.x + d.size/2) && mouseY > (d.y - d.size/2) && mouseY < (d.y + d.size/2) ) {
-      console.log(people.people[i]);
+      // Open the sidebar
+      select("#sidebar").elt.className = ''
+      sidebarClosed = false
 
-      //break the loop when the first circle is found (to avoid multiple selections on overlaps)
+      // Break the loop when the first circle is found (to avoid multiple selections on overlaps)
       break;
     }
+  }
+
+}
+
+function toggleSidebar(myClass) {
+  if (myClass != 'closed') {
+    sidebarClosed = false
+  } else {
+    sidebarClosed = true
+  }
+  select("#sidebar").elt.className = myClass;
+}
+
+function changeInformation(astro){
+  // Fill elements with the correct information
+  select("#astro-name").html(astro.name)
+  select('img',"#astro-img").elt.setAttribute('src',astro.biophoto)
+  select("#astro-role").html(astro.title)
+  select("#astro-elapsed").html(today.diff(moment(astro.launchdate),'days') + ' days')
+  select("#astro-ship").html(astro.location)
+  select('span','#astro-flag').elt.className = 'flag-icon flag-icon-'+astro.countryIso.toLowerCase()
+  if (astro.biolink != '') {
+    select('a',"#astro-bio").elt.setAttribute('href',astro.biolink)
+    selectAll(".astro-bio").forEach(function(element){
+      element
+        .style('display','')
+        .style('opacity','1')
+    })
+  } else {
+    selectAll(".astro-bio").forEach(function(element){
+      element
+        .style('display','none')
+        .style('opacity','0')
+    })
+  }
+  if (astro.twitter != '') {
+    select('a',"#astro-twitter").elt.setAttribute('href',astro.twitter)
+    select('a','#astro-twitter')
+      .html(astro.twitter.replace('http://twitter.com/',''))
+      .elt.setAttribute('href',astro.twitter)
+    selectAll(".astro-twitter").forEach(function(element){
+      element
+        .style('display','')
+        .style('opacity','1')
+    })
+  } else {
+    selectAll(".astro-twitter").forEach(function(element){
+      element
+        .style('display','none')
+        .style('opacity','0')
+    })
   }
 }
